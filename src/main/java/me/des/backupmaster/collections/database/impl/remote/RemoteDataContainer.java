1aeb8593d155156945220404842e164a448f9a86
Luxoru <luxoru.dev@gmail.com>
2023-08-12 22:07:21 +0100

    Added base remoteDataContainer

---
+++ src/main/java/me/des/backupmaster/collections/database/impl/remote/RemoteDataContainer.java
@@ -0,0 +1,57 @@
+package me.des.backupmaster.collections.database.impl.remote;
+
+import me.des.backupmaster.BackupMaster;
+import me.des.backupmaster.collections.database.DataContainer;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.concurrent.CompletableFuture;
+
+public abstract class RemoteDataContainer implements DataContainer {
+
+    protected final BackupMaster plugin;
+    protected String connectionString;
+    protected String database;
+    protected String collectionName;
+
+    protected final ConfigurationSection section;
+
+    public RemoteDataContainer(BackupMaster plugin, String type){
+        this.plugin = plugin;
+        this.section = plugin.getConfig().getConfigurationSection(type);
+        enable(section);
+    }
+
+
+    @Override
+    public CompletableFuture<Boolean> enable(ConfigurationSection section) {
+        return CompletableFuture.supplyAsync(() ->{
+            connectionString = section.getString("connection-string");
+
+            if (connectionString == null) {
+                String ip = section.getString("ip");
+                int port = section.getInt("port");
+                String authsource = section.getString("auth-source");
+                String username = section.getString("username");
+                String password = section.getString("password");
+                boolean ssl = section.getBoolean("ssl", false);
+                createConnectionString(ip, port, authsource, username, password, ssl);
+            }
+
+
+
+
+            try {
+                connect();
+                return true;
+            } catch (Exception expected) { // catching MongoException doesn't work for some reason
+                return false;
+            }
+        });
+    }
+
+    protected abstract void connect();
+
+    protected abstract String createConnectionString(String ip, int port, String authsource, String username, String password, boolean ssl);
+
+
+}
